bb29e7e4e3cdd03cba6e62a893ac1bf4c097b16d
Günther Theilen <gthln@posteo.de>
2025-12-16 06:44:38 +0100

    Add Swing

--- app/src/main/java/de/dentareport/Dentareport.java
+++ app/src/main/java/de/dentareport/Dentareport.java
@@ -1,11 +1,22 @@
 package de.dentareport;
 
+import javax.swing.*;
+
 public class Dentareport {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new Dentareport().getGreeting());
+        SwingUtilities.invokeLater(Dentareport::createAndShowGui);
+    }
+
+    private static void createAndShowGui() {
+        JFrame frame = new JFrame("Hello Swing");
+
+        JLabel label = new JLabel("Hello World!", SwingConstants.CENTER);
+
+        frame.add(label);
+        frame.setSize(300, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); // center on screen
+        frame.setVisible(true);
     }
 }
